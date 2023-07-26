37bf52a542c73014f67e4b793430b2242ce9a0fc
D B Vaghela <140400596+9429975145@users.noreply.github.com>
2023-07-26 05:53:12 -0700

    Update MAIN.py

--- MAIN.py
+++ MAIN.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 from streamlit_option_menu import option_menu
+import plotly.express as px
 
 st.image("SRGB_Logo.png",width=100)
 st.title("SAURASHTRA GRAMIN BANK")
